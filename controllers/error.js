f30b343ebb6f4437b37b6a6102d8d49f031d29e2
Aviv Doron <avivdm7@gmail.com>
2023-02-23 11:25:42 +0200

    sessions & cookies

--- controllers/error.js
+++ controllers/error.js
@@ -1,4 +1,9 @@
-exports.get404 =  (req, res, next) => {
-  res.status(404).render('404', {pageTitle: 'Page Not Found?!', path: req.url});
-}
-
+exports.get404 = (req, res, next) => {
+  res
+    .status(404)
+    .render("404", {
+      pageTitle: "Page Not Found?!",
+      path: req.url,
+      isAuthenticated: req.isLoggedIn
+    });
+};
